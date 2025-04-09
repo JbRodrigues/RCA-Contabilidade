c9e042b946f31a9b9f665e3576b412203f118f52
Julio Bisaia Rodrigues <42743827+JbRodrigues@users.noreply.github.com>
2025-04-09 17:06:49 +0000

    Adjust underline targets and button color on form contact section

--- assets/script/visuals.js
+++ assets/script/visuals.js
@@ -12,6 +12,7 @@ document.querySelector(".hero").addEventListener("mouseleave", function () {
     word.classList.remove("underline");
   });
 });
+
 /*-------------------------------------------------------------------------------*/
 document.querySelector(".help").addEventListener("mouseenter", function () {
   const targetWords = document.querySelectorAll(".underline-target1");
@@ -22,6 +23,21 @@ document.querySelector(".help").addEventListener("mouseenter", function () {
 
 document.querySelector(".help").addEventListener("mouseleave", function () {
   const targetWords = document.querySelectorAll(".underline-target1");
+  targetWords.forEach((word) => {
+    word.classList.remove("underline");
+  });
+});
+
+/*-------------------------------------------------------------------------------*/
+document.querySelector(".contact-form").addEventListener("mouseenter", function () {
+  const targetWords = document.querySelectorAll(".underline-target2");
+  targetWords.forEach((word) => {
+    word.classList.add("underline");
+  });
+});
+
+document.querySelector(".contact-form").addEventListener("mouseleave", function () {
+  const targetWords = document.querySelectorAll(".underline-target2");
   targetWords.forEach((word) => {
     word.classList.remove("underline");
   });
