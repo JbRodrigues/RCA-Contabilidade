79d949946e5faa49ebdeb1f370febd11488a1af6
Julio Bisaia Rodrigues <42743827+JbRodrigues@users.noreply.github.com>
2025-04-09 14:29:54 +0000

    Add underline on navbar, hero and help section

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,30 @@
-const form = document.getElementById('form');
-const formName = document.getElementById('name');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
+const form = document.getElementById("form");
+const formName = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Formulário enviado! Entraremos em contato em breve.');
-    formName.value = '';
-    email.value = '';
-    message.value = '';
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Formulário enviado! Entraremos em contato em breve.");
+  formName.value = "";
+  email.value = "";
+  message.value = "";
 });
 
+document
+  .querySelector(".hover-area")
+  .addEventListener("mouseenter", function () {
+    const targetWords = document.querySelectorAll(".underline-target");
+    targetWords.forEach((word) => {
+      word.classList.add("underline");
+    });
+  });
 
+document
+  .querySelector(".hover-area")
+  .addEventListener("mouseleave", function () {
+    const targetWords = document.querySelectorAll(".underline-target");
+    targetWords.forEach((word) => {
+      word.classList.remove("underline");
+    });
+  });
