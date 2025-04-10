c12d351da65d1620db457b59b35e847e40af288f
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2025-04-09 22:47:15 -0300

    Add About-us section and Adjust files

--- assets/script/visuals.js
+++ assets/script/visuals.js
@@ -42,3 +42,58 @@ document.querySelector(".contact-form").addEventListener("mouseleave", function 
     word.classList.remove("underline");
   });
 });
+
+/*-------------------------------------------------------------------------------*/
+document.querySelector("#about-card1").addEventListener("mouseenter", function () {
+  const targetWords = document.querySelectorAll(".underline-target3");
+  targetWords.forEach((word) => {
+    word.classList.add("underline3");
+  });
+});
+
+document.querySelector("#about-card1").addEventListener("mouseleave", function () {
+  const targetWords = document.querySelectorAll(".underline-target3");
+  targetWords.forEach((word) => {
+    word.classList.remove("underline3");
+  });
+});
+
+/*-------------------------------------------------------------------------------*/
+document.querySelector("#about-card2").addEventListener("mouseenter", function () {
+  const targetWords = document.querySelectorAll(".underline-target4");
+  targetWords.forEach((word) => {
+    word.classList.add("underline4");
+  });
+});
+
+document.querySelector("#about-card2").addEventListener("mouseleave", function () {
+  const targetWords = document.querySelectorAll(".underline-target4");
+  targetWords.forEach((word) => {
+    word.classList.remove("underline4");
+  });
+});
+
+/*-------------------------------------------------------------------------------*/
+document.querySelector("#about-card3").addEventListener("mouseenter", function () {
+  const targetWords = document.querySelectorAll(".underline-target5");
+  targetWords.forEach((word) => {
+    word.classList.add("underline5");
+  });
+});
+
+document.querySelector("#about-card3").addEventListener("mouseleave", function () {
+  const targetWords = document.querySelectorAll(".underline-target5");
+  targetWords.forEach((word) => {
+    word.classList.remove("underline5");
+  });
+});
+
+
+function toggleMenu() {
+  const nav = document.getElementById('navLinks');
+  nav.classList.toggle('show');
+}
+
+
+
+
