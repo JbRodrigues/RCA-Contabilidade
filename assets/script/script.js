6875b339e9f59c783e049a3d205e4d116bc12b91
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2025-04-24 20:45:55 -0300

    Adjust From section, add function to the form

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,4 @@
 const form = document.getElementById("form");
-const formName = document.getElementById("nome");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
 
